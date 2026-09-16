5c6206096f48cd455b80e81bf0f34c118e5551bf
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2026-09-16 14:39:09 +0300

    version: expose version vars for ldflags injection
    Expose Version and VersionPrerelease as package vars so goreleaser can stamp
    the released tag at build time, and derive PluginVersion from them instead of
    hardcoding 0.0.1. This makes released binaries report the actual tag version
    and keeps Packer's version constraint checks working.

--- version/version.go
+++ version/version.go
@@ -2,8 +2,23 @@ package version
 
 import sdkVersion "github.com/hashicorp/packer-plugin-sdk/version"
 
-// PluginVersion is the semantic version of the plugin, used for plugin set
-// registration and CLI describe output.
-//
-//nolint:gochecknoglobals
-var PluginVersion = sdkVersion.InitializePluginVersion("0.0.1", "")
+var (
+	// Version is the main version number that is being run at the moment.
+	// It is overwritten at build time via ldflags to match the released tag.
+	//
+	//nolint:gochecknoglobals
+	Version = "0.0.1"
+
+	// VersionPrerelease is a pre-release marker for the version. If this is ""
+	// (empty string) then it means that it is a final release. Otherwise, this
+	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
+	//
+	//nolint:gochecknoglobals
+	VersionPrerelease = ""
+
+	// PluginVersion is the semantic version of the plugin, used for plugin set
+	// registration and CLI describe output.
+	//
+	//nolint:gochecknoglobals
+	PluginVersion = sdkVersion.InitializePluginVersion(Version, VersionPrerelease)
+)
